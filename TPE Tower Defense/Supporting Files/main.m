aa179f82dd5ed7bf4a80aa901ed560db2f77e316
Franco Román Meola <franco.meola@icloud.com>
2014-04-10 11:34:59 -0300

    Initial Commit

---
+++ TPE Tower Defense/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TPE Tower Defense
+//
+//  Created by Franco Román Meola on 10/04/14.
+//  Copyright Franco Román Meola 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
