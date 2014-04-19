b77f568b3132a649547de3167722b7126013b8ed
Franco Román Meola <franco.meola@icloud.com>
2014-04-18 22:30:21 -0300

    Agregada una clase de Testeo de prueba.

---
+++ TPE Tower Defense Tests/TPE_Tower_Defense_Tests.m
@@ -0,0 +1,35 @@
+//
+//  TPE_Tower_Defense_Tests.m
+//  TPE Tower Defense Tests
+//
+//  Created by Franco Román Meola on 18/04/14.
+//  Copyright (c) 2014 Franco Román Meola. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TPE_Tower_Defense_Tests : XCTestCase
+
+@end
+
+@implementation TPE_Tower_Defense_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+/*
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+*/
+@end
